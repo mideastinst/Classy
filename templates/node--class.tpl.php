f9306baad5d12ec1f0b407d08fc55751270ddb03
Dan McEwan <dan.mcewan@gmail.com>
2012-05-14 16:50:11 -0400

    added registration validation handler v1

--- templates/node--class.tpl.php
+++ templates/node--class.tpl.php
@@ -17,14 +17,14 @@
   <?php endif; ?>
 
   <div class="content clearfix"<?php print $content_attributes; ?>>
-    TOTALLY WORKED
-    <?php print $registration; ?>
+   
     <?php
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
       print render($content);
     ?>
+    <?php print $registration; ?>
   </div>
 
   <?php
