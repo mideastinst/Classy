641325845bc26a354e8811f71a7befaac2fa60a3
mcewand <dan.mcewan@gmail.com>
2012-06-14 17:55:28 -0400

    updated sessions crediting

--- plugins/classy_civicrm/classy-vouchers.tpl.php
+++ plugins/classy_civicrm/classy-vouchers.tpl.php
@@ -4,15 +4,15 @@
   <?php foreach ($args as $id=>$type): ?>
     <li><?php print $type['name']; ?>
       <ul>
-        <?php if ($type['count']['unpaid'] != 0): ?>
+        <?php if (isset($type['count']) && $type['count']['unpaid'] != 0): ?>
           <li> Unpaid: <?php print $type['count']['unpaid']; ?> </li>
         <?php endif; ?>
-        <?php if ($type['count']['open'] != 0): ?>
+        <?php if (isset($type['count']) && $type['count']['open'] != 0): ?>
           <li>Available: <?php print $type['count']['open']; ?></li>
         <?php endif; ?>
-        <li><a href="/civicrm/contribute/transact?reset=1&id=<?php print $id; ?>">Purchase voucher</a></li>  
+        <li><a href="/civicrm/contribute/transact?reset=1&id=<?php print $type['contribution_page']; ?>">Purchase voucher</a></li>  
       </ul>
     </li>  
-  <?php endforeach; ?>
+  <?php endforeach; ?> 
   </ul>
 </div>
